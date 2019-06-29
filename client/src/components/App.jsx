dfa8d77c522a87d12ac92cd9d72a56d5737764ce
Ross Calimlim <ross.calimlim@gmail.com>
2019-06-29 18:03:51 -0500

    Create frontend and backend boilerplate

---
+++ client/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor () {
+    super();
+  }
+  render () {
+    return (<h1>Fix me!</h1>);
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: __dirname + '/client/src/index.jsx',
+
+  module: {
+    rules: [
+      { 
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  
+  output: {
+    path: __dirname + '/client/dist',
+    filename: 'bundle.js',
+    publicPath: '/'
+  }
+}
