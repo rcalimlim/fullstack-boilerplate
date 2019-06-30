48c5a6b2cbc99b165b5645ebf85835bd6ba88b0f
Ross Calimlim <ross.calimlim@gmail.com>
2019-06-29 19:42:07 -0500

    Complete server

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+  res.status(200).send('Get request successful!');
+});
 
 app.listen(port, () => console.log(`Server is running on ${port}`));
