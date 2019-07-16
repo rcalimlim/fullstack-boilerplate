f29f3093a76c3429bd33fef600b15790a16e9736
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-16 13:06:23 -0500

    Add css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,30 @@
 module.exports = {
   entry: __dirname + '/client/src/index.jsx',
 
+  mode: "production",
+
   module: {
     rules: [
-      { 
-        test: [/\.jsx$/],
-        exclude: /node_modules/,
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-react', '@babel/preset-env']
+            presets: [
+              ['@babel/preset-env', { modules: false }],
+              '@babel/preset-react',
+            ]
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
-  
+
   output: {
     path: __dirname + '/client/dist',
     filename: 'bundle.js',
